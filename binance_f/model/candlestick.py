7c5bd7d882df579cec8adcfbf60a6d9b6e7b5067
magnusknutas <magnus@thefarm.se>
2020-04-07 16:05:12 +0200

    All print replaced with logging, and some linting I really can't help it, its like tourettes

--- binance_f/model/candlestick.py
+++ binance_f/model/candlestick.py
@@ -4,7 +4,7 @@ class Candlestick:
         self.date = 0
         self.open = 0.0
         self.high = 0.0
-        self.lower = 0.0
+        self.low = 0.0
         self.close = 0.0
         self.volume = 0.0
         self.closeTime = 0
@@ -21,7 +21,7 @@ class Candlestick:
         result.date = val[0]
         result.open = val[1]
         result.high = val[2]
-        result.lower = val[3]
+        result.low = val[3]
         result.close = val[4]
         result.volume = val[5]
         result.closeTime = val[6]
